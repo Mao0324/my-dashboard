1cd41a78d1e9a6ff3d70f7f00239c2d8797596ff
MCong <1793252356@qq.com>
2025-11-22 21:31:12 +0800

    add tailwind css

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 关键修改：添加 base 属性，值必须是 '/仓库名/'
+  base: '/my-dashboard/',
+  
+  // 下面是可选的优化配置，帮你消除打包时的黄色警告
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
